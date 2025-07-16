8c3645d2b308fd55bd9419d9166126012b979fe9
Mangloo69 <yadav69.swayam@gmail.com>
2025-07-17 01:59:33 +0545

    Implement interveiw generation page ui

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -9,117 +9,114 @@ import { success } from "zod/v4-mini";
 const ONE_WEEK = 60 * 60 * 24 * 7;
 
 export async function signUp(params: SignUpParams) {
-    const { uid, name, email } = params;
+  const { uid, name, email } = params;
 
-    try {
-        const userRecord = await db.collection("users").doc(uid).get();
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
 
-        if (userRecord.exists) {
-            return {
-                success: false,
-                message: "User already exists. Please sign in instead.",
-            };
-        }
-
-        await db.collection("users").doc(uid).set({
-            name,
-            email,
-        });
-
-        return {
-            success: true,
-            message: "Account created successfully. Please sign in",
-        };
-    } catch (e: any) {
-        console.error("Error creating a user", e);
-
-        if (e.code === "auth/email-already-exists") {
-            return {
-                success: false,
-                message: "This email already in use",
-            };
-        }
-
-        return {
-            success: false,
-            message: "Failed to create an account",
-        };
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User already exists. Please sign in instead.",
+      };
     }
+
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in",
+    };
+  } catch (e: any) {
+    console.error("Error creating a user", e);
+
+    if (e.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email already in use",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Failed to create an account",
+    };
+  }
 }
 
 export async function signIn(params: SignInParams) {
-    const { email, idToken } = params;
+  const { email, idToken } = params;
 
-    try {
-        const userRecord = await auth().getUserByEmail(email);
+  try {
+    const userRecord = await auth().getUserByEmail(email);
 
-        if (!userRecord) {
-            return {
-                success: false,
-                message: "User does not exists. Create an account instead.",
-            };
-        }
-
-        await setSessionCookie(idToken);
-    } catch (e) {
-        console.log(e);
-
-        return {
-            success: false,
-            message: "Failed to log into an account",
-        };
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exists. Create an account instead.",
+      };
     }
+
+    await setSessionCookie(idToken);
+  } catch (e) {
+    console.log(e);
+
+    return {
+      success: false,
+      message: "Failed to log into an account",
+    };
+  }
 }
 
 export async function setSessionCookie(idToken: string) {
-    const cookieStore = await cookies();
+  const cookieStore = await cookies();
 
-    const sessionCookie = await auth().createSessionCookie(idToken, {
-        expiresIn: ONE_WEEK * 1000,
-    });
+  const sessionCookie = await auth().createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000,
+  });
 
-    cookieStore.set("session", sessionCookie, {
-        maxAge: ONE_WEEK,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        path: "/",
-        sameSite: "lax",
-    });
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
 }
 
 export async function getCurrentUser(): Promise<User | null> {
-    const cookieStore = await cookies();
+  const cookieStore = await cookies();
 
-    const sessionCookie = cookieStore.get("session")?.value;
+  const sessionCookie = cookieStore.get("session")?.value;
 
-    if (!sessionCookie) return null;
+  if (!sessionCookie) return null;
 
-    try {
-        const decodedClaims = await auth().verifySessionCookie(
-            sessionCookie,
-            true
-        );
+  try {
+    const decodedClaims = await auth().verifySessionCookie(sessionCookie, true);
 
-        const userRecord = await db
-            .collection("users")
-            .doc(decodedClaims.uid)
-            .get();
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
 
-        if (!userRecord.exists) return null;
+    if (!userRecord.exists) return null;
 
-        return {
-            ...userRecord.data(),
-            id: userRecord.id,
-        } as User;
-    } catch (e) {
-        console.log(e);
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (e) {
+    console.log(e);
 
-        return null;
-    }
+    return null;
+  }
 }
 
 export async function isAuthenticated() {
-    const user = await getCurrentUser();
+  const user = await getCurrentUser();
 
-    return !!user;
+  return !!user;
 }
